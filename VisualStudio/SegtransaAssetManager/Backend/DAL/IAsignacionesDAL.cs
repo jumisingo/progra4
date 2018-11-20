d3b1f5f9a96fa67bf521ed8249fb2d4b8e93a57e
Christian villaobos <cvv81@hotmail.com>
2018-11-19 19:37:32 -0600

    Entities + Interfaces

---
+++ VisualStudio/SegtransaAssetManager/Backend/DAL/IAsignacionesDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Entities;
+using Backend.DAL;
+
+namespace Backend.DAL
+{
+    public interface ICategoriaActivosDAL : IDisposable
+    {
+        void Add(CategoriaActivos CategoriaActivo);
+        void Delete(int idCategoriaActivo);
+        void Update(CategoriaActivos CategoriaActivo);
+        CategoriaActivos GetCategoriaActivo(int idCategoriaActivo);
+        List<CategoriaActivos> GetCategoriaActivos();
+    }
+}
