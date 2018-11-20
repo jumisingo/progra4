11d4279f8e43a08b60628a512ee9215045d2e60f
Christian villaobos <cvv81@hotmail.com>
2018-11-19 20:56:05 -0600

    FrontEnd name space / UnitTest name space

---
+++ VisualStudio/SegtransaAssetManager/UnitTests/UnitTestActivos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Backend.DAL;
+using Backend.Entities;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestActivos
+    {
+        [TestMethod]
+        public void TestMethodAdd()
+        {
+            IActivosDAL activosDAL = new ActivosImplDAL();
+            int count = activosDAL.GetActivos().Count;
+
+            Activos activo = new Activos
+            {
+
+            };
+        }
+    }
+}
---
+++ VisualStudio/SegtransaAssetManager/UnitTests/UnitTestProveedores.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Backend.DAL;
+using Backend.Entities;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestProveedores
+    {
+        [TestMethod]
+        public void TestMethodAdd()
+        {
+            IProveedoresDAL proveedoresDAL = new ProveedoresImplDAL();
+            int count = proveedoresDAL.GetProveedores().Count;
+
+            Proveedores proveedor = new Proveedores
+            {
+                idProveedor = 100001,
+                nombre = "test",
+                telefono = "+506-2266-8996",
+                correo = "test@proveedor.com",
+                direccion = "San Jose, Costa Rica",
+            };
+            proveedoresDAL.Add(proveedor);
+            Assert.AreEqual(count + 1, proveedoresDAL.GetProveedores().Count);
+        }
+    }
+}
