f3684ddda3a65111109c470145ae97883999289b
Melissa Villalta <mevillaltas@hotmail.com>
2018-12-11 10:10:50 -0600

    Form UsuariosModifica

--- VisualStudio/SegtransaAssetManager/FrontEnd/frmUsuarios.Designer.cs
+++ VisualStudio/SegtransaAssetManager/FrontEnd/frmUsuarios.Designer.cs
@@ -30,12 +30,15 @@
         {
             this.btnBack = new System.Windows.Forms.Button();
             this.btnAddUser = new System.Windows.Forms.Button();
+            this.lblListaUsuarios = new System.Windows.Forms.Label();
+            this.lstUsuarios = new System.Windows.Forms.ListBox();
+            this.btnUpdateUser = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnBack
             // 
             this.btnBack.Location = new System.Drawing.Point(532, 332);
-            this.btnBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(2);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(58, 24);
             this.btnBack.TabIndex = 7;
@@ -45,25 +48,58 @@
             // 
             // btnAddUser
             // 
-            this.btnAddUser.Location = new System.Drawing.Point(61, 59);
+            this.btnAddUser.Location = new System.Drawing.Point(45, 266);
             this.btnAddUser.Name = "btnAddUser";
-            this.btnAddUser.Size = new System.Drawing.Size(75, 23);
+            this.btnAddUser.Size = new System.Drawing.Size(118, 32);
             this.btnAddUser.TabIndex = 8;
             this.btnAddUser.Text = "Agregar Usuario";
             this.btnAddUser.UseVisualStyleBackColor = true;
             this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
+            // 
+            // lblListaUsuarios
+            // 
+            this.lblListaUsuarios.AutoSize = true;
+            this.lblListaUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblListaUsuarios.Location = new System.Drawing.Point(45, 26);
+            this.lblListaUsuarios.Name = "lblListaUsuarios";
+            this.lblListaUsuarios.Size = new System.Drawing.Size(122, 31);
+            this.lblListaUsuarios.TabIndex = 9;
+            this.lblListaUsuarios.Text = "Usuarios";
+            // 
+            // lstUsuarios
+            // 
+            this.lstUsuarios.FormattingEnabled = true;
+            this.lstUsuarios.Location = new System.Drawing.Point(51, 77);
+            this.lstUsuarios.Name = "lstUsuarios";
+            this.lstUsuarios.Size = new System.Drawing.Size(481, 160);
+            this.lstUsuarios.TabIndex = 10;
+            // 
+            // btnUpdateUser
+            // 
+            this.btnUpdateUser.Location = new System.Drawing.Point(220, 266);
+            this.btnUpdateUser.Name = "btnUpdateUser";
+            this.btnUpdateUser.Size = new System.Drawing.Size(118, 32);
+            this.btnUpdateUser.TabIndex = 11;
+            this.btnUpdateUser.Text = "Modificar Usuario";
+            this.btnUpdateUser.UseVisualStyleBackColor = true;
+            this.btnUpdateUser.Click += new System.EventHandler(this.btnUpdateUser_Click);
             // 
             // frmUsuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnUpdateUser);
+            this.Controls.Add(this.lstUsuarios);
+            this.Controls.Add(this.lblListaUsuarios);
             this.Controls.Add(this.btnAddUser);
             this.Controls.Add(this.btnBack);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmUsuarios";
             this.Text = "Usuarios";
+            this.Load += new System.EventHandler(this.frmUsuarios_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +107,8 @@
 
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.Label lblListaUsuarios;
+        private System.Windows.Forms.ListBox lstUsuarios;
+        private System.Windows.Forms.Button btnUpdateUser;
     }
 }
--- VisualStudio/SegtransaAssetManager/FrontEnd/frmUsuarios.cs
+++ VisualStudio/SegtransaAssetManager/FrontEnd/frmUsuarios.cs
@@ -14,14 +14,19 @@ namespace FrontEnd
 {
     public partial class frmUsuarios : Form
     {
+        private IUsuariosDAL usuariosDAL;
+        private List<Usuarios> Usuarios;
+
         public frmUsuarios()
         {
             InitializeComponent();
+            usuariosDAL = new UsuariosImplDAL();
         }
         public frmUsuarios(Form prevForm)
         {
             InitializeComponent();
             previousForm = prevForm;
+            usuariosDAL = new UsuariosImplDAL();
         }
 
         static Form previousForm;
@@ -33,10 +38,43 @@ namespace FrontEnd
             previousForm.Show();
         }
 
+        public void CargarLista()
+        {
+            lstUsuarios.Items.Clear();
+            Usuarios = usuariosDAL.GetUsuarios();
+           
+            //lstUsuarios.DisplayMember = "apellido1";
+
+            foreach(var item in Usuarios)
+            {
+                item.nombre = item.nombre.PadRight(20) + item.apellido1.PadRight(20) +
+                    item.apellido2.PadRight(20);
+            }
+
+            lstUsuarios.DisplayMember = "nombre";
+            lstUsuarios.ValueMember = "idUsuario";
+
+            lstUsuarios.DataSource = Usuarios;
+
+        }
+
         private void btnAddUser_Click(object sender, EventArgs e)
         {
             frmUsuariosAgrega frm_UsuariosAgrega = new frmUsuariosAgrega(this);
             frm_UsuariosAgrega.Show();
+            this.Hide();
+        }
+
+        private void frmUsuarios_Load(object sender, EventArgs e)
+        {
+            CargarLista();
+        }
+
+        private void btnUpdateUser_Click(object sender, EventArgs e)
+        {
+            Usuarios Usuario = (Usuarios)lstUsuarios.SelectedItem;
+            frmUsuariosModifica frm_UsuariosModifica = new frmUsuariosModifica(this);
+            frm_UsuariosModifica.Show();
             this.Hide();
         }
     }
